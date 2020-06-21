511c198342a3705042423b1807758d185d572bd5
Daniel Kim <tkkscf@gmail.com>
2020-06-21 03:20:11 -0500

    Updates

---
+++ precache-manifest.3d5a5294ab5de0213ed0deb36357f62f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a021827077025082a4e0441db3265f",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "09c76a1ba2c5e60d0004",
+    "url": "/react-hooks/static/css/main.bb4c944e.chunk.css"
+  },
+  {
+    "revision": "bd59519e37c7424d2cd2",
+    "url": "/react-hooks/static/js/2.45e94fd1.chunk.js"
+  },
+  {
+    "revision": "09c76a1ba2c5e60d0004",
+    "url": "/react-hooks/static/js/main.fc9be9c6.chunk.js"
+  },
+  {
+    "revision": "650cad77a29d2fea9fb1",
+    "url": "/react-hooks/static/js/runtime~main.b9448681.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.a3e033c7e651e6d9a0134214e1de1690.js"
+  "/react-hooks/precache-manifest.3d5a5294ab5de0213ed0deb36357f62f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
